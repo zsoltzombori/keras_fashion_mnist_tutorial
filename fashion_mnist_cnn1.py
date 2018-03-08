14b99e30add194ea09a49764e3b36f0729ce5283
Zsolt Zombori <zsolt.zombori@t-online.hu>
2018-03-08 17:14:08 +0100

    sample code for training on fashion_mnist using keras

---
+++ fashion_mnist_cnn1.py
@@ -0,0 +1,105 @@
+# importing required libraries
+import keras
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Lambda
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+
+# set random seed
+np.random.seed(12345)
+
+# Load Fashion-MNIST
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+# convert brightness values from bytes to floats between 0 and 1:
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# save last 20% from x_train, y_train for validation set (for model tuning)
+val_size = len(X_train) / 5
+X_val = X_train[-val_size:]
+y_val = y_train[-val_size:]
+X_train = X_train[:-val_size]
+y_train = y_train[:-val_size]
+
+# add extra channel dimension
+X_train = np.expand_dims(X_train, 3)
+X_test = np.expand_dims(X_test, 3)
+X_val = np.expand_dims(X_val, 3)
+
+print "Train data shape ", X_train.shape
+print "Train label shape ", y_train.shape
+print "Validation data shape ", X_val.shape
+print "Validation label shape ", y_val.shape
+print "Test data shape ", X_test.shape
+print "Test label shape ", y_test.shape
+
+INPUT_SHAPE=X_train.shape[1:]
+BATCH_SIZE = 512
+
+# CNN with 1 Convolutional Layer
+model = Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=INPUT_SHAPE),
+    MaxPooling2D(pool_size=(2, 2)),
+    Dropout(0.2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy',
+             optimizer=Adam(lr=0.001),
+             metrics=['accuracy'])
+
+model.fit(X_train, y_train,
+         batch_size=BATCH_SIZE,
+         epochs=10,
+         verbose=1,
+         validation_data=(X_val, y_val))
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# ('Test loss:', 0.27444312539100646)
+# ('Test accuracy:', 0.8987)
+
+# Reduce the learning rate and continue training
+model.optimizer.lr = 0.0001
+
+model.fit(X_train, y_train,
+         batch_size=BATCH_SIZE,
+         epochs=10,
+         verbose=1,
+         validation_data=(X_val, y_val))
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# ('Test loss:', 0.24485005620718003)
+# ('Test accuracy:', 0.9151)
+
+
+# Use data augmentation and continue training
+gen = ImageDataGenerator(rotation_range=8, width_shift_range=0.08, shear_range=0.3,
+                               height_shift_range=0.08, zoom_range=0.08)
+batches = gen.flow(X_train, y_train, batch_size=BATCH_SIZE)
+val_batches = gen.flow(X_val, y_val, batch_size=BATCH_SIZE)
+
+model.fit_generator(batches, steps_per_epoch=48000//BATCH_SIZE, epochs=50, 
+                   validation_data=val_batches, validation_steps=12000//BATCH_SIZE)
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#('Test loss:', 0.2488910299241543)
+#('Test accuracy:', 0.9144)
---
+++ fashion_mnist_dense.py
@@ -0,0 +1,59 @@
+# importing required libraries
+import keras
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.optimizers import Adam
+
+# Load Fashion-MNIST
+(X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+# convert brightness values from bytes to floats between 0 and 1:
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# save last 20% from x_train, y_train for validation set (for model tuning)
+val_size = len(X_train) / 5
+X_val = X_train[-val_size:]
+y_val = y_train[-val_size:]
+X_train = X_train[:-val_size]
+y_train = y_train[:-val_size]
+
+print "Train data shape ", X_train.shape
+print "Train label shape ", y_train.shape
+print "Validation data shape ", X_val.shape
+print "Validation label shape ", y_val.shape
+print "Test data shape ", X_test.shape
+print "Test label shape ", y_test.shape
+
+INPUT_SHAPE=X_train.shape[1:]
+BATCH_SIZE = 512
+
+# Two layer dense network
+model = Sequential([
+    Flatten(input_shape=INPUT_SHAPE),
+    Dense(512, activation='relu'),
+    Dense(128, activation = 'relu'),
+    Dense(10, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer=Adam(lr=0.001),
+               loss='sparse_categorical_crossentropy',
+               metrics=['accuracy'])
+
+history = model.fit(X_train, y_train,
+                     batch_size=BATCH_SIZE,
+                     epochs=20,
+                     verbose=1,
+                     validation_data=(X_val, y_val))
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# ('Test loss:', 0.33005201976299287)
+# ('Test accuracy:', 0.8925)
